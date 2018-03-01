2ef6c1e2784df93eea606c7dc17cb52ce18ce0db
kadenjenson <sobeit99@gmail.com>
2018-03-01 15:05:01 -0700

    Mounted Action cable for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,11 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = [ 'https://bear-canyon-construction.herokuapp.com', 'http://bear-canyon-construction.herokuapp.com' ]
+  config.action_cable.url = "wss://bear-canyon-construction.herokuapp.com/cable"
 end
+
+
+
+
